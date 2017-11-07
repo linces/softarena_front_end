f59771ec49a03fbb98b31987df3ddb9ce1602f35
Linces Marques <linces@gmail.com>
2017-11-07 19:45:03 -0200

    Correção index.php
    Signed-off-by: Linces Marques <linces@gmail.com>

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 <!doctype html>
-=======
-<<!doctype html>
->>>>>>> ef5c38d42bb02ee3e0b732f74072480992e3a0ec
+
 <html>
-    <head>
+     <head>
         <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="description" content="SoftArena">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>.: SoftArena :.</title>
@@ -13,11 +11,12 @@
         <link rel="author" href="humans.txt">
     </head>
     <body>
-<<<<<<< HEAD
+
        <script src="js/main.js"></script>
-=======
+
         
         <script src="js/main.js"></script>
->>>>>>> ef5c38d42bb02ee3e0b732f74072480992e3a0ec
+
     </body>
 </html>
+
